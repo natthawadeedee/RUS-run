2fc1a0cb9e79be4541a23e03d1656db94e90a485
Natthawadee <Natthawadee7045@mail.com>
2016-07-13 16:08:19 +0700

    show Logo

--- app/src/main/java/rus/natthawadee/rusrun/MainActivity.java
+++ app/src/main/java/rus/natthawadee/rusrun/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private EditText userEditText, passwoedEditText;
+    private ImageView imageView;
+    private static final String urllogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Bion Widget
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwoedEditText = (EditText) findViewById(R.id.editText5);
+        imageView = (ImageView) findViewById(R.id.imageView6);
+
+                // Load image from Server
+        Picasso.with(this).load(urllogo).into(imageView);
+
     }// Main Method
 
     public void clickSingUpMain(View view) {

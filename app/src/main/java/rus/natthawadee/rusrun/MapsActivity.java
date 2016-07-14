60b83de84aa5b2050faa99ac6be2ad5d547f177a
Natthawadee <Natthawadee7045@mail.com>
2016-07-14 16:08:58 +0700

    map okok

--- app/src/main/java/rus/natthawadee/rusrun/MapsActivity.java
+++ app/src/main/java/rus/natthawadee/rusrun/MapsActivity.java
@@ -13,6 +13,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double latRusADouble = 13.859132;
+    private double lngARusDouble = 100.481989;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +27,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     } //Main Meth
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //Set Center Map
+        LatLng latLng = new LatLng(latRusADouble,lngARusDouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
     } // onmapReady
 }//Main class
